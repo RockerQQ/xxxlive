c93d665b314f467092ac3b6cfe06170ca94d3f1e
liuxiangrong <liuxiangrong@youmi.net>
2015-08-20 11:34:53 +0800

    you are rich

---
+++ build/xxx.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+	var a=100,b=80;
+	function beRich(){
+		alert('you rich now?');
+}
+	var bb='no bb';
+	function beBitch(){
+		alert('you are a bitch now');
+}
+}
+
+var a=1000;
+alert(a++);
